fa055d33c2a3aac253ad19b80e844231e990f945
Gajy <Gaji1@yandex.rugit config user.emailgit config --global user.email Gaji1@yandex.ru>
2023-01-27 10:22:15 +0300

    Completed work

--- test/Program.cs
+++ test/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+string[] arrString;
+arrString = new string[4];
+
+
+static void FilArr(string[] arrString)
+    {
+  
+      for (int i = 0; i < arrString.Length; i++)
+      {
+        Console.WriteLine("Введите строку:");
+        string s = Console.ReadLine();
+        if(s.Length < 4)
+          arrString[i] = s;
+      }
+      
+    }
+    
+    
+     void PrintArr(string[] arrString)
+    {
+  
+      for (int i = 0; i < arrString.Length; i++)
+      {
+        if(arrString[i] != null)
+         Console.Write($" {arrString[i]}, ");
+      }
+    }
+    
+    FilArr(arrString);
+    Console.WriteLine();
+    Console.Write("["); PrintArr(arrString);  Console.Write("]");
+  
+
+    
