876db992bad762c61df8b89c09a13ca16e999855
Gajy <Gaji1@yandex.rugit config user.emailgit config --global user.email Gaji1@yandex.ru>
2023-01-27 14:05:07 +0300

    fixed

--- test/Program.cs
+++ test/Program.cs
@@ -11,7 +11,6 @@ static void FilArr(string[] arrString)
       {
         Console.WriteLine("Введите строку:");
         string s = Console.ReadLine();
-        if(s.Length < 4)
           arrString[i] = s;
       }
       
@@ -23,14 +22,32 @@ static void FilArr(string[] arrString)
   
       for (int i = 0; i < arrString.Length; i++)
       {
-        if(arrString[i] != null)
+        if(i < arrString.Length - 1)
          Console.Write($" {arrString[i]}, ");
+
+         if(i == arrString.Length - 1)
+         Console.Write($" '{arrString[i]}' ");
       }
+    }
+
+    static void PrintArrB(string[] arrString)
+    {
+  
+      for (int i = 0; i < arrString.Length; i++)
+      {
+        if(arrString[i].Length < 4)
+        {
+          Console.Write($" '{arrString[i]}' ");
+        }
+      }
+      
     }
     
     FilArr(arrString);
     Console.WriteLine();
     Console.Write("["); PrintArr(arrString);  Console.Write("]");
+    Console.WriteLine();
+    Console.Write("["); PrintArrB(arrString);  Console.Write("]");
   
 
     
